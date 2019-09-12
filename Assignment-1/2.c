2f96e18cc2c71cfc6232265de85014b7604e988c
rohit162 <54887902+rohit162@users.noreply.github.com>
2019-09-12 22:56:41 +0530

    Add files via upload

---
+++ Assignment-1/2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int a,b,output;
+printf("enter the values of a,b");
+scanf("%d%d",&a,&b);
+output=a*a+b*b*b;
+printf("the value of output is %d",output);
+}
---
+++ Assignment-1/3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,p,output;
+printf("enter the values of a,b,c,p");
+scanf("%d%d%d%d",&a,&b,&c,&p);
+output=a*(p*p)+(b*p)+c;
+printf("the output is %d",output);
+}
